bb29ff31bd94c95a6e94ee79385781fecd3d9543
Sedanur Çevik <sdnrcvk@gmail.com>
2023-07-14 23:35:14 +0300

    Newsletter, Testimonial, SubAbout entities created

---
+++ EntityLayer/Concrete/Testimonial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Testimonial
+    {
+        public int TestimonialId { get; set; }
+        public string Client { get;}
+        public string Comment { get;}
+        public string ClientImage { get;}
+        public bool Status { get; set; }
+    }
+}
