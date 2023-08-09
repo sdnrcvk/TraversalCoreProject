7b4eb03c68c1dab196f8d36d7c4a892383d5f1c4
Sedanur Çevik <sdnrcvk@gmail.com>
2023-08-10 02:40:52 +0300

    DestinationController created, view created for destinations page

---
+++ TraversalCoreProject/Controllers/DestinationController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Controllers
+{
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IActionResult Index()
+        {
+            var destinations=destinationManager.TGetAll();
+            return View(destinations);
+        }
+    }
+}
