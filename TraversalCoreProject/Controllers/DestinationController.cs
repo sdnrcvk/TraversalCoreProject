3effcb6e9e2b756376159c2ac1527aa72acd4b87
Sedanur Çevik <sdnrcvk@gmail.com>
2023-08-11 00:35:21 +0300

    View created for DestinationDetails

--- TraversalCoreProject/Controllers/DestinationController.cs
+++ TraversalCoreProject/Controllers/DestinationController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProject.Controllers
@@ -11,6 +12,18 @@ namespace TraversalCoreProject.Controllers
         {
             var destinations=destinationManager.TGetAll();
             return View(destinations);
+        }
+
+        [HttpGet]
+        public IActionResult DestinationDetails(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DestinationDetails(Destination p) 
+        {
+            return View();
         }
     }
 }
