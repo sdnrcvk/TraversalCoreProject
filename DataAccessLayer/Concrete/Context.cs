be87a9dc70cf368b918f4b764d92bbc8ba4d3e7a
Sedanur Çevik <sdnrcvk@gmail.com>
2023-08-19 00:12:25 +0300

    Comment entity created - relationship established with destination entity - service, manager, dal created for comments

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -21,5 +21,6 @@ namespace DataAccessLayer.Concrete
         public DbSet<Newsletter> Newsletters { get; set; }
         public DbSet<SubAbout> SubAbouts { get; set; }
         public DbSet<Testimonial> Testimonials { get; set; }
+        public DbSet<Comment> Comments { get; set; }
     }
 }
--- DataAccessLayer/Repository/GenericRepository.cs
+++ DataAccessLayer/Repository/GenericRepository.cs
@@ -3,6 +3,7 @@ using DataAccessLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -33,6 +34,12 @@ namespace DataAccessLayer.Repository
         {
             using var c=new Context();
             return c.Set<T>().Find(id);
+        }
+
+        public List<T> GetListByFilter(Expression<Func<T, bool>> filter)
+        {
+            using var c = new Context();
+            return c.Set<T>().Where(filter).ToList();
         }
 
         public void Update(T t)
