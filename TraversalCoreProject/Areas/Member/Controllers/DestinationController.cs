f5c7d12928073b4e379ce310d88f08c5922db84d
Sedanur Çevik <sdnrcvk@gmail.com>
2023-09-06 01:10:47 +0300

    Areas and member layout created

---
+++ TraversalCoreProject/Areas/Member/Controllers/DestinationController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Areas.Member.Controllers
+{
+    [Area("Member")]
+    [AllowAnonymous]
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+
+        public IActionResult Index()
+        {
+            var destinations = destinationManager.TGetAll();
+            return View(destinations);
+        }
+    }
+}
--- TraversalCoreProject/Program.cs
+++ TraversalCoreProject/Program.cs
@@ -43,4 +43,12 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
+
 app.Run();
