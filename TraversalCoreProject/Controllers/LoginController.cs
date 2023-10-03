19f77df4c897dedc15bf2f42dcde3866ee1cda74
Sedanur Çevik <sdnrcvk@gmail.com>
2023-10-04 00:35:41 +0300

    Login with identity package

--- TraversalCoreProject/Controllers/LoginController.cs
+++ TraversalCoreProject/Controllers/LoginController.cs
@@ -10,13 +10,15 @@ namespace TraversalCoreProject.Controllers
 	public class LoginController : Controller
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
 
-		public LoginController(UserManager<AppUser> userManager)
-		{
-			_userManager = userManager;
-		}
+        public LoginController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
 
-		[HttpGet]
+        [HttpGet]
         public IActionResult SignUp()
         {
             return View();
@@ -53,6 +55,24 @@ namespace TraversalCoreProject.Controllers
         [HttpGet]
         public IActionResult SignIn()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(UserSignInViewModel p)
+        {
+            if(ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(p.Username, p.Password, false, true);
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Destination");
+                }
+                else
+                {
+                    return RedirectToAction("SignIn", "Login");
+                }
+            }
             return View();
         }
     }
