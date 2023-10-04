dd6280cd116b1a78d08d0e2375b116322692a63e
Sedanur Çevik <sdnrcvk@gmail.com>
2023-10-04 03:17:57 +0300

    profile & picture information updated with identity

--- TraversalCoreProject/Areas/Member/Controllers/ProfileController.cs
+++ TraversalCoreProject/Areas/Member/Controllers/ProfileController.cs
@@ -28,5 +28,30 @@ namespace TraversalCoreProject.Areas.Member.Controllers
             userEditViewModel.Mail = values.Email;
             return View(userEditViewModel);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditViewModel p)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            if (p.Image != null)
+            {
+                var resource = Directory.GetCurrentDirectory();
+                var extension= Path.GetExtension(p.Image.FileName);
+                var imageName = Guid.NewGuid() + extension;
+                var saveLocation = resource + "/wwwroot/userimages/" + imageName;
+                var stream=new FileStream(saveLocation,FileMode.Create);
+                await p.Image.CopyToAsync(stream);
+                user.ImageUrl = imageName;
+            }
+            user.Name=p.Name;
+            user.Surname=p.Surname;
+            user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, p.Password);
+            var result=await _userManager.UpdateAsync(user);
+            if(result.Succeeded)
+            {
+                return RedirectToAction("SignIn", "Login");
+            }
+            return View();
+        }
     }
 }
