8063b2e4e811f3f2c3f3295bace0dbe23009996c
Sedanur Çevik <sdnrcvk@gmail.com>
2023-10-22 00:27:35 +0300

    profile template added for dashboard page

---
+++ TraversalCoreProject/Areas/Member/Controllers/DashboardController.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Areas.Member.Controllers
+{
+    [Area("Member")]
+    public class DashboardController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public DashboardController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.fullName=values.Name+" "+values.Surname;
+            ViewBag.userImage = values.ImageUrl;
+            return View();
+        }
+    }
+}
