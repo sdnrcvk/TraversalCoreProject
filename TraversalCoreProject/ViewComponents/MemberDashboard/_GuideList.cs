295daea0bd7839ff9c6e16ecbac76aca57f29d8f
Sedanur Çevik <sdnrcvk@gmail.com>
2023-11-02 00:50:19 +0300

    tour guides list part designed

---
+++ TraversalCoreProject/ViewComponents/MemberDashboard/_GuideList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.MemberDashboard
+{
+    public class _GuideList:ViewComponent
+    {
+        GuideManager guideManager = new GuideManager (new EfGuideDal());
+        public IViewComponentResult Invoke()
+        {
+            var guides = guideManager.TGetAll();
+            return View(guides);
+        }
+    }
+}
