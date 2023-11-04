bb07f163ca92c8fa73bc2f240826c5f5db257582
Sedanur Çevik <sdnrcvk@gmail.com>
2023-11-04 23:25:43 +0300

    admin layout partial views created

--- TraversalCoreProject/Controllers/AdminController.cs
+++ TraversalCoreProject/Controllers/AdminController.cs
@@ -13,5 +13,23 @@ namespace TraversalCoreProject.Controllers
         {
             return PartialView();
         }
+
+        public PartialViewResult PartialSideBar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
     }
 }
