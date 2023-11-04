9986dce14b57e1a8178c3e9e56a09136666256f0
Sedanur Çevik <sdnrcvk@gmail.com>
2023-11-05 02:49:31 +0300

    admin dashboard divided into view components

---
+++ TraversalCoreProject/ViewComponents/AdminDashboard/_Card1Statistic.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.AdminDashboard
+{
+    public class _Card1Statistic:ViewComponent
+    {
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.destinationNumber = c.Destinations.Count();
+            ViewBag.guestNumber=c.Users.Count();
+            return View();
+        }
+    }
+}
