d94720cdc3613f95937f731b414a7c41f6df0294
Sedanur Çevik <sdnrcvk@gmail.com>
2023-11-24 01:58:59 +0300

    guides listed

---
+++ TraversalCoreProject/Controllers/GuideController.cs
@@ -0,0 +1,55 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Controllers
+{
+    public class GuideController : Controller
+    {
+        private readonly IGuideService _guideService;
+
+        public GuideController(IGuideService guideService)
+        {
+            _guideService = guideService;
+        }
+
+        public IActionResult Index()
+        {
+            var guides = _guideService.TGetAll();
+            return View(guides);
+        }
+
+        [HttpGet]
+        public IActionResult AddGuide()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddGuide(Guide guide)
+        {
+            _guideService.TAdd(guide);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult EditGuide(int id)
+        {
+            var values=_guideService.TGetById(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult EditGuide(Guide guide)
+        {
+            _guideService.TUpdate(guide);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult ChangeTheStatus(int id)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
