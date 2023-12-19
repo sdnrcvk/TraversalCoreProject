c7d7c7c23b8609cf70a7ce90c65a9638f9115e6a
Sedanur Çevik <sdnrcvk@gmail.com>
2023-12-19 04:06:49 +0300

    email sending completed

---
+++ TraversalCoreProject/Areas/Admin/Controllers/MailController.cs
@@ -0,0 +1,42 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using TraversalCoreProject.Models;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(MailRequest mailRequest)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("Admin","softwaredeveloper.en@gmail.com");
+            mimeMessage.From.Add(mailboxAddressFrom);
+
+            MailboxAddress mailboxAddressTo= new MailboxAddress("User", mailRequest.ReceiverMail);
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = mailRequest.Body;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject = mailRequest.Subject;
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com", 587, false);
+            client.Authenticate("softwaredeveloper.en@gmail.com", "kodgelecek");
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+
+            return View();
+        }
+    }
+}
