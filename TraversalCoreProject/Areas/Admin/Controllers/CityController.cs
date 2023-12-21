17d10dfd0b66e7a1e15ddf7c5779601a40308536
Sedanur Çevik <sdnrcvk@gmail.com>
2023-12-22 00:26:10 +0300

    ajax operations 1

---
+++ TraversalCoreProject/Areas/Admin/Controllers/CityController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TraversalCoreProject.Models;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CityList()
+        {
+            var jsonCity = JsonConvert.SerializeObject(cities);
+            return Json(jsonCity);
+        }
+
+        public static List<CityClass> cities = new List<CityClass>
+        {
+            new CityClass
+            {
+                CityId=1,
+                CityName="Üsküp",
+                CityCountry="Makedonya"
+            },
+            new CityClass
+            {
+                CityId= 2,
+                CityName="Roma",
+                CityCountry="İtalya"
+            },
+            new CityClass
+            {
+                CityId = 3,
+                CityName="Londra",
+                CityCountry="İngiltere"
+            }
+        };
+    }
+}
