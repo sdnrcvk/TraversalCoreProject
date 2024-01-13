f483e5d891044438a3a969c9014b3157237bde04
Sedanur Çevik <sdnrcvk@gmail.com>
2024-01-13 22:59:31 +0300

    getting a hotel list via rapid api Booking

---
+++ TraversalCoreProject/Areas/Admin/Controllers/BookingHotelSearchController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using TraversalCoreProject.Areas.Admin.Models;
+using System.Net.Http.Headers;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class BookingHotelSearchController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://booking-com.p.rapidapi.com/v2/hotels/search?room_number=1&adults_number=2&checkout_date=2024-05-20&filter_by_currency=EUR&units=metric&locale=tr&checkin_date=2024-05-19&dest_type=city&dest_id=-1456928&order_by=popularity&children_ages=5%2C0&children_number=2&include_adjacency=true&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&page_number=0"),
+                Headers =
+                {
+                    { "X-RapidAPI-Key", "66887f8858msh0bfeb80a5223e15p19869cjsn177e2ba503bd" },
+                    { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+                },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(body);
+                var values = JsonConvert.DeserializeObject<BookingHotelSearchViewModel>(body);
+                return View(values.results);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCityDestID()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetCityDestID(string p)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://booking-com.p.rapidapi.com/v1/hotels/locations?name={p}&locale=en-gb"),
+                Headers =
+                {
+                    { "X-RapidAPI-Key", "66887f8858msh0bfeb80a5223e15p19869cjsn177e2ba503bd" },
+                    { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+                },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(body);
+                return View();
+            }
+        }
+    }
+}
