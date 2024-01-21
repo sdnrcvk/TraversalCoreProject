c209d3b51c34b2301d68d3d135ca5686e24f1f86
Sedanur Çevik <sdnrcvk@gmail.com>
2024-01-21 21:35:55 +0300

    unit of work implementation

---
+++ DataAccessLayer/Abstract/IGenericUowDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericUowDal<T> where T : class
+    {
+        void Add(T t);
+        void Update(T t);
+        void MultiUpdate(List<T> list);
+        T GetById(int id);
+    }
+}
