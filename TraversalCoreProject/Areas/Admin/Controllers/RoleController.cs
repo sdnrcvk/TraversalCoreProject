4711ad0956d5bbe3b70f14b9085253445ea9a950
Sedanur Çevik <sdnrcvk@gmail.com>
2024-01-31 15:19:30 +0300

    project role list and role adding operations

---
+++ TraversalCoreProject/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,48 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TraversalCoreProject.Areas.Admin.Models;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public RoleController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var values=_roleManager.Roles.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel createRoleViewModel)
+        {
+            AppRole role = new AppRole()
+            {
+                Name = createRoleViewModel.RoleName
+            };
+            var result=await _roleManager.CreateAsync(role);
+            if(result.Succeeded)
+            {
+                return RedirectToAction("Role","Admin");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
