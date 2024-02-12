dea7f747c52bc84f55bcae19ea326f7a88bb755d
Sedanur Çevik <sdnrcvk@gmail.com>
2024-02-13 02:30:03 +0300

    forgot my password and reset password page added

--- TraversalCoreProject/Program.cs
+++ TraversalCoreProject/Program.cs
@@ -4,6 +4,7 @@ using EntityLayer.Concrete;
 using FluentValidation.AspNetCore;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.AspNetCore.Mvc.Razor;
 using System.Reflection;
@@ -32,7 +33,7 @@ builder.Services.AddLogging(x =>
 });
 
 builder.Services.AddDbContext<Context>();
-builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>().AddErrorDescriber<CustomIdentityValidator>().AddEntityFrameworkStores<Context>();
+builder.Services.AddIdentity<AppUser, AppRole>().AddTokenProvider<DataProtectorTokenProvider<AppUser>>(TokenOptions.DefaultProvider).AddEntityFrameworkStores<Context>().AddErrorDescriber<CustomIdentityValidator>().AddEntityFrameworkStores<Context>();
 
 builder.Services.AddHttpClient();
 
