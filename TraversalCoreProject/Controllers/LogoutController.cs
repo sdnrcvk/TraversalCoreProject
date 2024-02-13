c6244d7d5a04b136853f1fdabf12bc4c8da83dc9
Sedanur Çevik <sdnrcvk@gmail.com>
2024-02-13 20:34:16 +0300

    logout operation added

---
+++ TraversalCoreProject/Controllers/LogoutController.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Controllers
+{
+    public class LogoutController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public LogoutController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index","Default");
+        }
+    }
+}
